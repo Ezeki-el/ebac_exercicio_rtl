29411ff59691d8a639f88e3c92c9ab9581d0d110
Ezequiel Oliveira Barboza <132308977+Ezeki-el@users.noreply.github.com>
2024-07-02 18:59:17 -0300

    Add files via upload

--- src/components/PostComments/PostComments.tsx
+++ src/components/PostComments/PostComments.tsx
@@ -1,3 +1,30 @@
-import { PostComments } from './PostComments.1';
+    import React, { useState } from 'react';
 
-export default PostComments;
+    const PostComments = () => {
+    const [comments, setComments] = useState<string[]>([]);
+    const [comment, setComment] = useState<string>('');
+
+    const handleSubmit = () => {
+        setComments([...comments, comment]);
+        setComment('');
+    };
+
+    return (
+        <div>
+        <input
+            data-testid="comment-input"
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+        <ul>
+            {comments.map((comment, index) => (
+            <li key={index}>{comment}</li>
+            ))}
+        </ul>
+        </div>
+    );
+    };
+
+    export default PostComments;
